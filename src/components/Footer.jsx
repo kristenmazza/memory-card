865e9211c340fa02759f27b2ccf72f8e5f3c355f
kristenmazza <kbmazza@gmail.com>
2023-07-26 11:52:11 -0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,14 @@ import './Footer.css';
 
 export default function Footer() {
     return (
-        <footer className="footer">Footer</footer>
+        <footer className="footer">
+            <div className="created-by">
+                <p>created by kristenmazza</p>
+                <a href="https://github.com/kristenmazza"><img className="icon" src='/src/images/github-icon.png' /> </a>
+            </div>
+            <div className="credits">
+                speech bubbles by <a href="https://www.freepik.com/free-vector/colorful-speech-bubble_892831.htm#page=2&query=colorful%20speech%20bubbles&position=33&from_view=search&track=ais">Freepik</a>
+            </div>
+        </footer >
     )
 }
