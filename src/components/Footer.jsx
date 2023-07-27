1b52e79a9db07afb63956667f0754ed5ea87953b
kristenmazza <kbmazza@gmail.com>
2023-07-26 17:02:53 -0700

    Update image in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="footer">
             <div className="created-by">
                 <p>created by kristenmazza</p>
-                <a href="https://github.com/kristenmazza"><img className="icon" src='/src/images/github-icon.png' /> </a>
+                <a href="https://github.com/kristenmazza"><img className="icon" src='/images/github-icon.png' /> </a>
             </div>
             <div className="credits">
                 speech bubbles by <a href="https://www.freepik.com/free-vector/colorful-speech-bubble_892831.htm#page=2&query=colorful%20speech%20bubbles&position=33&from_view=search&track=ais">Freepik</a>
